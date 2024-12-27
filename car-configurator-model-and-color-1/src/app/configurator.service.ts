1add0ff53002fd5a3c6d26f4ce6e1589b28940a6
Walter Velardo <waltervelardo@gmail.com>
2024-12-27 19:01:49 +0100

    Add .gitignore and update configurator service for model and color selection

--- car-configurator-model-and-color-1/src/app/configurator.service.ts
+++ car-configurator-model-and-color-1/src/app/configurator.service.ts
@@ -1,18 +1,31 @@
-import {inject, Injectable, signal, Signal} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {toSignal} from '@angular/core/rxjs-interop';
-import {CarModel} from './models.type';
+import { inject, Injectable, signal, Signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CarModel, Color } from './models.type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConfiguratorService {
-
   private http = inject(HttpClient);
   readonly allModels: Signal<CarModel[]> = toSignal(
-    this.http.get<CarModel[]>("models"), {initialValue: []}
+    this.http.get<CarModel[]>('models'),
+    { initialValue: [] }
   );
 
   readonly currentCar = signal<CarModel | undefined>(undefined);
+  readonly currentColor = signal<Color | undefined>(undefined);
 
+  selectModel(code: CarModel['code']) {
+    const model = this.allModels()?.find((model) => model.code === code);
+    this.currentCar.set(model);
+    this.currentColor.set(model?.colors[0]);
+  }
+
+  selectColor(code: Color['code']) {
+    const color = this.currentCar()?.colors.find(
+      (color) => color.code === code
+    );
+    this.currentColor.set(color);
+  }
 }
