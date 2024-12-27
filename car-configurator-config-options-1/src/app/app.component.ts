071452ed94686fdbb6ef1ae79d7e10b2136161c6
Walter Velardo <waltervelardo@gmail.com>
2024-12-27 19:29:40 +0100

    Implement routing for steps and update configurator service for step readiness

--- car-configurator-config-options-1/src/app/app.component.ts
+++ car-configurator-config-options-1/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import {Component, inject} from '@angular/core';
-import {Step1Component} from './step1/step1.component';
-import {ConfiguratorService} from './configurator.service';
+import { Component, inject } from '@angular/core';
+import { Step1Component } from './step1/step1.component';
+import { ConfiguratorService } from './configurator.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Step1Component],
-  templateUrl: "app.component.html",
+  imports: [RouterOutlet, RouterLink],
+
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   service = inject(ConfiguratorService);
-
 }
--- car-configurator-config-options-1/src/app/app.routes.ts
+++ car-configurator-config-options-1/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'step1', component: Step1Component },
+  { path: 'step2', component: Step2Component },
+];
